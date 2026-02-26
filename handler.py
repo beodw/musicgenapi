a01055ab50f3b75d99a6b00d03b5f93e38e399c4
beodw <beodwilson@gmail.com>
2026-02-26 18:25:39 +0000

    remove os.exit

--- handler.py
+++ handler.py
@@ -111,7 +111,6 @@ def handler(job):
         torch.cuda.empty_cache() 
         import gc
         gc.collect()
-        os._exit(0)
 
 init_pipeline()
 runpod.serverless.start({"handler": handler})
